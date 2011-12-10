525a712d134f5ae96be9d9b705797ae49226f1ba
Binbin Cao <fatcamera@gmail.com>
2011-12-10 19:40:17 +0800

    some fix

--- extension.js
+++ extension.js
@@ -26,7 +26,6 @@ Indicator.prototype = {
 		this._verse.clutter_text.line_wrap = true;
 		this._verse.clutter_text.line_wrap_mode = Pango.WrapMode.WORD_CHAR;
 		this._verse.clutter_text.ellipsize = Pango.EllipsizeMode.NONE;
-		this._verse.clutter_text.set_markup('<span>Loading ...</span>');
 		let verseArea = new St.Bin({style_class: 'verse-area', x_align: St.Align.MIDDLE});
 		verseArea.set_child(this._verse);
 		let verseMenuItem = new PopupMenu.PopupMenuSection();
@@ -46,7 +45,6 @@ Indicator.prototype = {
         for (key in BIBLE_VERSION) {
 			let button = new St.Button({ label: key, style_class: 'version-button' });
 			button.connect('clicked', Lang.bind(this, function (sender) {
-				//this._version = sender.label;
 				for (j in BIBLE_VERSION) {
 					if (sender === this._versions[j]) this._versions[j].add_style_class_name('current');
 					else this._versions[j].remove_style_class_name('current');
@@ -72,9 +70,13 @@ Indicator.prototype = {
         let session = new Soup.SessionAsync();
         let message = Soup.Message.new('GET', url);
         session.queue_message(message, function(session, message) {
-            let jp = new Json.Parser();
-            jp.load_from_data(message.response_body.data, -1);
-            callback.call(here, jp.get_root().get_object());
+			if (message.status_code == 200){
+				let jp = new Json.Parser();
+				jp.load_from_data(message.response_body.data, -1);
+				callback.call(here, jp.get_root().get_object());
+			} else {
+				callback.call(here, null);
+			}
         });
     },
     
@@ -83,18 +85,24 @@ Indicator.prototype = {
 	},
 	
     refresh_verse: function(version) {
+		this._verse.set_text('loading ...');
+		this._ref.set_text('');
 		this.load_json_async(this.get_url(version), function(json_data) {
-			global.log('json gotted');
-			let votd = json_data.get_object_member('votd');
-			let text = votd.get_string_member('text');
-			text = text.replace(/^\&ldquo\;/g, '')
-				.replace(/\&rdquo\;$/g, '')
-				.replace(/\&\#(\d*)\;[\ ]*/g, function(str, p1) { // &#32822
-					return String.fromCharCode(p1);
-			});
-			this._verse.clutter_text.set_markup('<span gravity="east">' + text + '</span>');
-			let display_ref = votd.get_string_member('display_ref');
-			this._ref.clutter_text.set_markup('<i>' + display_ref + '</i>');
+			if (json_data == null){
+				this._verse.set_text('operation failed');
+				this._ref.set_text('');
+			} else {
+				let votd = json_data.get_object_member('votd');
+				let text = votd.get_string_member('text')
+					.replace(/^\&ldquo\;/g, '')
+					.replace(/\&rdquo\;$/g, '')
+					.replace(/\&\#(\d*)\;[\ ]*/g, function(str, p1) { // &#32822
+						return String.fromCharCode(p1);
+				});
+				this._verse.set_text(text);
+				let display_ref = votd.get_string_member('display_ref');
+				this._ref.set_text(display_ref);
+			}
 		});
 	}
 };
